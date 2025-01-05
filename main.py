526e0e4d1d71874a18b581bbb98afea13c3710fb
Flayme <166822467+FlaymeXDD@users.noreply.github.com>
2025-01-05 02:15:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -26,59 +26,92 @@ def url_pars():
             servers = [server for server in full_info['servers'] if server.get('region') != 'eu']
             servers = sorted(servers, key=lambda server: int(server['id'][2:]))
             return servers
-        return []
-        
+    return []  # Return empty list if no servers are found
+
 async def masterlist_embed():
-    servers = url_pars() 
+    servers = url_pars()
 
     if servers:
         total_players = 0
+        icon = ""
+
         for server in servers:
             players = server.get('players')
             total_players += players
 
         embed = discord.Embed(
-            title = f"Majestic Masterlist [Онлайн: {total_players}]",
-            color = int("E81C5A", 16),
+            title = f"Общий онлайн: {total_players:,.0f}".replace(",", " "),
+            color=int("E81C5A", 16),
         )
-        # Устанавливаем иконку для футера
-        embed.set_thumbnail(
-            url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fyt3.ggpht.com%2Fa%2FAATXAJwzTgrfBJV9YCDB0tMuXy0e4-yMIsb-_WQeMw%3Ds900-c-k-c0xffffffff-no-rj-mo&f=1&nofb=1&ipt=6555cdac49409ace8b08916427a7c5b92fbcf25823dc413a4cfdf60b23b1c7d3&ipo=images" 
-        )
-    for server in servers:
+
+        for server in servers:
             name = server.get('name')
-            ip = server.get('ip')
             players = server.get('players')
             max_players = server.get('maxPlayersHardLimit')
             status = server.get('status')
 
-            total_players += players
-
-            if status == True:
-                status_info = ":white_check_mark:"
+            if status:
+                status_info = "<:Checkmark:1325243634588586045>"
             else:
-                status_info = ":no_entry:"
+                status_info = "<:trialmoderator:1325243800234233897>"
 
-            embed.add_field(
-                name=f"**{name}** |  {status_info}",
-                value=f"**Онлайн:** `{players}/{max_players}`",
-                inline=False
+            if name == 'New York':
+                icon = "<:ru1:1325245154524921856>"
+            elif name == 'Detroit':
+                icon = "<:ru2:1325245156039069726>" 
+            elif name == 'Chicago':
+                icon = "<:ru3:1325245158018777181>"  
+            elif name == 'San Francisco':
+                icon = "<:ru4:1325245159260291132>"  
+            elif name == 'Atlanta':
+                icon = "<:ru5:1325245160380043274>"  
+            elif name == 'San Diego':
+                icon = "<:ru6:1325238312478183514>"
+            elif name == 'Los Angeles':
+                icon = "<:ru7:1325245163433492480>"  
+            elif name == 'Miami':
+                icon = "<:ru8:1325245165341900971>"  
+            elif name == 'Las Vegas':
+                icon = "<:ru9:1325245190214254602>"  
+            elif name == 'Washington':
+                icon = "<:ru10:1325245168902869032>"      
+            elif name == 'Dallas':
+                icon = "<:ru11:1325247054959611954>"  
+            elif name == 'Boston':
+                icon = "<:ru12:1325247445709492265>"  
+            elif name == 'Houston':
+                icon = "<:RU13:1325247254193373326>"  
+
+            embed.set_author(
+                    name="Majestic Masterlist",
+                    icon_url = "https://yt3.googleusercontent.com/tQ4uUwYvV0qwDs_Q51NUNUB0ypgR-Spf5JAJbNfejnoh9n9WDl6isvS-hM3GAozz3XLAHjDw9Rs=s900-c-k-c0x00ffffff-no-rj" 
             )
             
-            time = datetime.now().strftime("%d %B %H:%M")
-
-            embed.set_footer(
-                text = f"Информация обновлена  •  {time}"
+            embed.add_field(
+                name=f"**{name}**  {icon}\n**Статус сервера:** {status_info}\tㅤ",
+                value=f"**Онлайн:** `{players}/{max_players}`\nㅤ",
+                inline=True
             )
 
-    channel = bot.get_channel(channelid)
-    if channel:
-            async for message in channel.history(limit=1):
-                await message.edit(embed=embed)
-                return
-            await channel.send(embed=embed)
+            time = datetime.now().strftime("%d %B  %H:%M")
+            embed.set_footer(
+                text=f"Информация обновлена  •  {time}"
+            )
 
-# Запускаем периодическую отправку обновлений
+        channel = bot.get_channel(channelid)
+        if channel:
+            async for message in channel.history(limit=1):
+                if message.author == bot.user and message.embeds:  # Проверка на наличие эмбеда
+                    try:
+                        await message.edit(embed=embed)  # Обновление существующего эмбеда
+                        return 
+                    except discord.errors.Forbidden:
+                        await channel.send(embed=embed)  # Если нельзя редактировать, отправляем новый
+                        return
+            await channel.send(embed=embed)  # Отправка нового эмбеда, если его нет
+
+
+# Periodically send updates
 @bot.event
 async def on_ready():
     while True:
@@ -86,4 +119,3 @@ async def on_ready():
         await asyncio.sleep(5)
 
 bot.run(token)
-
